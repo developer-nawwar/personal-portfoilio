c026c94454dbf3ab89ff977e38f409b6a9dad26d
developer-nawwar <nawwarkhaddaj@gmail.com>
2022-05-02 23:37:14 +0300

    changing navbar background on scroll

--- js/script.js
+++ js/script.js
@@ -2,4 +2,22 @@ $(document).ready(function() {
 
     /*change the header height*/
     $(".background").height($(window).height());
+    //change nav bar backgroud when scrolling
+    $(this).scroll(function() {
+        var top = $(".intro").offset().top;
+        if ($(this).scrollTop() >= top) {
+            console.log("mm");
+            $("nav").addClass("nav-bg");
+        } else {
+            console.log('vv');
+            $("nav").removeClass("nav-bg");
+        }
+
+
+
+
+    });
+
+
+
 });
